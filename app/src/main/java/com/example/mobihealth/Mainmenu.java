51ce2b0de4463402d41dd3fe5363d48a1f53ab6d
sbhandari0817 <santosh.bhandari2@mavs.uta.edu>
2020-11-26 14:09:53 -0600

    android11/26

--- app/src/main/java/com/example/mobihealth/Mainmenu.java
+++ app/src/main/java/com/example/mobihealth/Mainmenu.java
@@ -2,13 +2,30 @@ package com.example.mobihealth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Mainmenu extends AppCompatActivity {
-
+    private ImageView PI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+
+        setupUI();
+
+        PI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Mainmenu.this,Personal_Information.class));
+            }
+        });
+    }
+
+    void setupUI(){
+        PI = (ImageView) findViewById(R.id.PIclick);
     }
 }
